c8945528161cee3a6c583ec23580fbb8ded484ca
Mauricio Robayo <rfmajo@gmail.com>
2020-08-15 19:27:53 -0500

    refactor: use trm-api package

--- src/useApi.js
+++ src/useApi.js
@@ -1,3 +1,4 @@
+import TrmApi from 'trm-api';
 import { useState, useEffect } from 'react';
 
 function useApi() {
@@ -5,34 +6,32 @@ function useApi() {
   const [state, setData] = useState([]);
 
   useEffect(() => {
-    const url = 'https://www.datos.gov.co/resource/32sa-8pi3.json?$limit=30';
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedData = data
-          .reduce((acc, e, i) => {
-            const startDate = new Date(e.vigenciadesde);
-            const endDate = new Date(e.vigenciahasta);
-            while (endDate >= startDate) {
-              acc.push({
-                value: e.valor,
-                date: endDate.toISOString(),
-              });
+    const trmapi = new TrmApi();
+    trmapi.history({ limit: 30, order: 'DESC' }).then((data) => {
+      const transformedData = data
+        .reduce((acc, e, i) => {
+          const startDate = new Date(e.vigenciadesde);
+          const endDate = new Date(e.vigenciahasta);
+          while (endDate >= startDate) {
+            acc.push({
+              value: e.valor,
+              date: endDate.toISOString(),
+            });
 
-              endDate.setDate(endDate.getDate() - 1);
-            }
-            return acc;
-          }, [])
-          .map((e, i, arr) => {
-            if (i + 1 < arr.length) {
-              e.change = e.value - arr[i + 1].value;
-              e.percChange = e.change / e.value;
-            }
-            return e;
-          });
-        setData(transformedData);
-        setIsLoading(false);
-      });
+            endDate.setDate(endDate.getDate() - 1);
+          }
+          return acc;
+        }, [])
+        .map((e, i, arr) => {
+          if (i + 1 < arr.length) {
+            e.change = e.value - arr[i + 1].value;
+            e.percChange = e.change / e.value;
+          }
+          return e;
+        });
+      setData(transformedData);
+      setIsLoading(false);
+    });
   }, []);
   return {
     isLoading,
